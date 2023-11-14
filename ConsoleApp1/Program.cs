80df3abb68c18069cb8842cde05afa7aa13d8d08
Timok951 <timofey.alekseev@list.ru>
2023-11-14 20:36:50 +0300

    pushing project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+ 
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static DriveInfo[] GetAvailableDrives()
+        {
+            return DriveInfo.GetDrives();
+        }
+
+        private static void DisplayDriveInfo(DriveInfo drive)
+        {
+            Console.WriteLine($"Drive: {drive.Name}");
+            Console.WriteLine($"Total Space: {drive.TotalSize / (1024 * 1024 * 1024)} GB");
+            Console.WriteLine($"Used Space: {drive.TotalSize - drive.AvailableFreeSpace / (1024 * 1024 * 1024)} GB");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("File manager choose disk");
+                var drives = GetAvailableDrives();
+                for(int i =0; i < drives.Length; i++)
+                {
+                    Console.WriteLine($"  {i + 1}. {drives[i].Name}");
+                    
+                }
+                Menu.Show(1, drives.Length);
+            }
+
+        }
+    }
+}
